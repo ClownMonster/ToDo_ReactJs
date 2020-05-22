456a78e94ee7c215c662776b3d106c9f4f48975f
ClownMonster <mk6386223@gmail.com>
2020-05-22 10:10:15 +0530

    check box done

--- src/components/Todocomp.js
+++ src/components/Todocomp.js
@@ -4,20 +4,39 @@ import PropTypes from 'prop-types';
 export class Todocomp extends Component {
 
     getStyle = () =>
-    {
-        if(this.props.todo.completed)
-        {
-            return {color:"blue"}
+    {   
+        return{
+        background:this.props.todo.completed ? ' #c0392b ': ' #1a5276 ' ,
+        padding:'0.4rem',
+        border: '1px solid  #979a9a ',
+        boxShadow: '2px 2px 2px 1px black'
+
+
+
         }
-        else return{ color:"red"}
-        
     }
 
 
+    input_style = () =>{
+        return{
+            cursor:'pointer',
+            border:'2px solid white'
+
+        }
+    }
+
+
+
     render() {
+
+        const {id, title } = this.props.todo;
         return (
             <div style = {this.getStyle()} >
-                <p>{this.props.todo.title}</p>
+                <p>
+                    <input type="checkbox" onChange={this.props.mark_complete.bind(this,id)} style = {this.input_style()} />{ '   ' }
+                    {title}
+                    
+                </p>
             </div>
         )
     }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 export class Todos extends React.Component {
     render() {
         return this.props.todos.map((todo) =>
-            <Todocomp key={todo.id} todo={todo} />
+            <Todocomp key={todo.id} todo={todo} mark_complete = {this.props.markComplete} />
         );
     }
 
